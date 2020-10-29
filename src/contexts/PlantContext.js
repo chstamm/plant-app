343977603a4f123ff79e7d0245b8168035eb2443
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-10-28 18:39:54 -0600

    Added back text data for design and Auth

--- src/contexts/PlantContext.js
+++ src/contexts/PlantContext.js
@@ -1,12 +1,13 @@
 import React, { useContext, createContext, useState, useEffect } from "react";
-import axios from 'axios'
-
+//import axios from 'axios'
+//Below is for Design Testing Only 
+import plantdata from "/Users/chelseastamm/Sites/React/Plants/plant-app/src/Components/plants.json"
 const PlantContext = createContext({ plants: [] });
 
-export const PlantContextProvider = (props) => {
+ export const PlantContextProvider = (props) => {
   const [plants, setPlants] = useState([]);
 
-   useEffect(() => {
+ /*   useEffect(() => {
     const apiKey = process.env.REACT_APP_TREFLE_PLANT_API_KEY;
     console.log(process.env);
     const getPlantData = async () => {
@@ -21,8 +22,12 @@ export const PlantContextProvider = (props) => {
       }
     };
     getPlantData();
-  }, []); 
-
+  }, []);  */
+  
+  //Below is for Design Testing Only 
+useEffect(() => {
+    setPlants(plantdata.data)
+  }, [])
 
   return (
     <div>
